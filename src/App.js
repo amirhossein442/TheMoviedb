837226e669d654c0b00a283f56d592f5bfd372bc
Amirhossein <amirhossein.442@gmail.com>
2025-09-20 22:58:52 +0330

    main

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Header } from "./component/Header/header";
 import { Sliders2 } from "./component/Slider-Latest Trailers/Slider2";
 import { Sliders1 } from "./component/Slider-Trending/slider";
 import { Sliders3 } from "./component/Slider-What's Popular/slider";
-//install css
+
 
 function App() {
   return (
